d32d6cd185828ca5e07e7737160d04d4835f964c
Tran Tuan <xuantuanbmt753@gmail.com>
2023-04-14 17:25:09 +0700

    Detail user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CustomAuthController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//AllUser ten phuong thuc
+Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+
+//AllUser ten phuong thuc
+Route::get('/view', [UserController::class, 'AllUser'])->name('all.user');
+Route::get('/detail/{id}', [UserController::class, 'DetailUser'])->name('detail.user');
+// Route::get('/edit', [UserController::class, 'EditUser'])->name('edit.user');
+Route::get('/delete/user/{id}', [UserController::class], 'DeleteUser')->name('delete.user');
